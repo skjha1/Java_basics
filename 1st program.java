9f51bf292255b516264f9e17437a51c37aaef0a2
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2023-01-01 21:08:31 +0530

    Create 1st program.java

---
+++ 1st program.java
@@ -0,0 +1,8 @@
+ 1 : System.out.print("Hello World");
+   2 : System.out.print("Hello World");
+   3 : System.out.print("Hello Shivendra")
+   4 : System.out.print("Hello! How are you doing")
+   8 : System.out.print("myFirstNumber");
+   9 : System.out.print(myFirstNumber);
+  10 : int myFirstNumber = 1000;
+  11 : System.out.print(myFirstNumber);
