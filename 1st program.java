4c4097071fe1b8dc2a51c38a465641938e829311
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2023-01-01 23:25:40 +0530

    Update 1st program.java

--- 1st program.java
+++ 1st program.java
@@ -1,8 +1,17 @@
- 1 : System.out.print("Hello World");
+1 : System.out.print("Hello World");
    2 : System.out.print("Hello World");
    3 : System.out.print("Hello Shivendra")
    4 : System.out.print("Hello! How are you doing")
    8 : System.out.print("myFirstNumber");
    9 : System.out.print(myFirstNumber);
-  10 : int myFirstNumber = 1000;
   11 : System.out.print(myFirstNumber);
+  12 : myFirstNumber = 10 + 5 + 8;
+  13 : myFirstNumber = (10 +5) + (2 * 10);
+  14 : int myFirstNumber = (10 + 5) + (2 * 10);
+  15 : int mySecondNumber = 12;
+  16 : int myThirdNumber = 6;
+  17 : int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+  18 : myThirdNumber = myFirstNumber * 2
+  19 : myTotal = myFirstNumber + mySecondNumber + myThirdNumber
+  20 : int myLastOne = 1000 - myTotal;
+  21 : System.out.print(myLastOne)
